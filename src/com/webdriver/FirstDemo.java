43a0bffa5e9c85a48b627314967e09fd280824e7
ajay jagtap <jagtapajay64@gmail.com>
2024-07-17 11:19:34 +0530

    New

--- src/com/webdriver/FirstDemo.java
+++ src/com/webdriver/FirstDemo.java
@@ -1,6 +1,7 @@
 package com.webdriver;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class FirstDemo {
@@ -20,6 +21,21 @@ public class FirstDemo {
 		
 //		driver.close();// to close a tab
 		driver.quit();// to close all tab
+		
+		System.setProperty("webdriver.chrome.driver","E:\\Tops 2024\\Selenium\\chromedriver.exe");
+		//to open empty browser
+		WebDriver driver1=new ChromeDriver();
+		
+		//to open a website
+		driver1.get("https://www.w3schools.com/html/");
+		
+		Thread.sleep(2000);
+		System.out.println("url is :"+driver1.getCurrentUrl());
+		Thread.sleep(2000);
+		System.out.println("title is :"+driver1.getTitle());
+		
+//		driver.close();// to close a tab
+		driver1.quit();// to close all tab
 	}
 
 }
