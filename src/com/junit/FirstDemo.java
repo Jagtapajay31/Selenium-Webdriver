dbbc9743dc0a6ee8cba1e3a373f15032825da222
ajay jagtap <jagtapajay64@gmail.com>
2024-08-07 10:44:38 +0530

    new

--- src/com/junit/FirstDemo.java
+++ src/com/junit/FirstDemo.java
@@ -1,5 +1,12 @@
 package com.junit;
-
+/*
+ * What is Junit?
+ * JUnit is an open source Unit Testing Framework for JAVA.
+ * It is useful for Java Developers to write and run repeatable tests.
+ * Erich Gamma and Kent Beck initially develop it.
+ * It is an instance of xUnit architecture.
+ * As the name implies, it is used for Unit Testing of a small chunk of code.
+ */
 import static org.junit.Assert.fail;
 
 import org.junit.Test;
