a5ab1a1317cc4f23c7007a5959c83f5860e643af
ajay jagtap <jagtapajay64@gmail.com>
2024-08-13 11:55:32 +0530

    new

---
+++ src/com/junit/ArithmeticExample.java
@@ -0,0 +1,8 @@
+package com.junit;
+
+public class ArithmeticExample {
+	public void div(int a,int b) {
+		System.out.println("div is :"+a/b);
+		System.out.println("your division is ssucessfull");
+	}
+}
