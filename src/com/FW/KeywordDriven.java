63f0a4858846ddf75057e6680e900c42f54e1b58
ajay jagtap <jagtapajay64@gmail.com>
2024-09-04 10:59:19 +0530

    new

---
+++ src/com/FW/KeywordDriven.java
@@ -0,0 +1,84 @@
+package com.FW;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class KeywordDriven {
+	@DataProvider(name="tops")
+	public Object[][] readData() throws InvalidFormatException, IOException {
+		Object[][] data = null;
+		//1.to get a filepath
+		String filepath= "C:\\Users\\os\\OneDrive\\Desktop\\DataDriven.xlsx";
+		//2.to make file
+		File file = new File(filepath);
+		//3.to open a excel file
+		XSSFWorkbook workbook= new XSSFWorkbook(file);
+		//4.to open a sheet
+		Sheet sheet= workbook.getSheet("sheet2");
+		//5.to select a row
+		int n_row=sheet.getPhysicalNumberOfRows();
+		System.out.println("no of row is :"+n_row);
+		data=new Object[n_row][];
+		for (int i = 0; i < data.length; i++) {
+			Row row=sheet.getRow(i);
+			
+			//6.to get select col
+			int n_col=row.getPhysicalNumberOfCells();
+			System.out.println("no of col is :"+n_col);
+			data[i]=new String[n_col];
+			for (int j = 0; j < data[i].length; j++) {
+				
+				
+				Cell cell=row.getCell(j);
+				//7.to convert all value into string
+				cell.setCellType(CellType.STRING);
+				
+				//8.to get value from the cell
+				data[i][j]=cell.getStringCellValue();
+			}		
+		}
+		
+		return data;
+	}
+
+	WebDriver driver=null;
+	@Test(dataProvider = "tops")
+	public void test(String keyword) throws InterruptedException, InvalidFormatException, IOException {
+//		Object[][] data=readData();
+		
+//		for (int i = 0; i < data.length; i++) {
+		
+		if (keyword.equalsIgnoreCase("open browser")) {
+		System.setProperty("webdriver.chrome.driver","E:\\Tops 2024\\Selenium\\chromedriver.exe");
+		 	driver=new ChromeDriver();
+		}else if (keyword.equalsIgnoreCase("enter url")) {
+			driver.get("https://www.saucedemo.com/");
+			Thread.sleep(2000);
+		}else if (keyword.equalsIgnoreCase("enter username")) {
+			driver.findElement(By.name("user-name")).sendKeys("standard_user");
+			Thread.sleep(2000);
+		}else if (keyword.equalsIgnoreCase("enter password")) {
+			driver.findElement(By.name("password")).sendKeys("secret_sauce");
+			Thread.sleep(2000);
+		}else if (keyword.equalsIgnoreCase("click login")) {
+			driver.findElement(By.name("login-button")).click();
+			Thread.sleep(2000);
+		}else if (keyword.equalsIgnoreCase("close browser")) {
+			driver.close();
+		}
+	}
+
+//}
+}
